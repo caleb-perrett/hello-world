d7193648bf063b25cd9ba1987f5c3d1b1051f4b1
theboysie <57600110+theboysie@users.noreply.github.com>
2020-07-29 12:40:43 +1200

    Create main.py
    This is just the base file for the FLASK project

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template, request, redirect, url_for, flash, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, MetaData, Table
+
+
+
+from datetime import date
+import os
+
+
+import datetime
+
+
+#import flask_sqlalchemy
+#import SQLalchemy
+
+
+
+from flask import g
+
+DATABASE = 'C:/Users/chris/Documents/GitHub/spa/Flask/Database/thelaw.db'
+todaysdate = date.today()
+
+
+import sqlite3
+legalAidUploadFolder = 'C:/Users/chris/Documents/GitHub/spa/Flask/static/docs/legalAid/'
+casesrootfolder = 'C:\\Users\\chris\\Documents\\GitHub\\spa\\Flask\\static\\docs\\'
+ALLOWED_EXTENSIONS  = {'.pdf'}
+DATABASE = 'C:/Users/Chris/PycharmProjects/jobcosting/nutrition.db'
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = 'super secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Chris/PycharmProjects/jobcosting/nutrition.db'
+# app.config['UPLOAD_FOLDER'] = legalAidUploadFolder
+db = SQLAlchemy(app)
+
+
+engine = create_engine('sqlite:///C:/Users/Chris/PycharmProjects/jobcosting/nutrition.db')
+db.Model.metadata.reflect(db.engine)
+metadata = MetaData(bind=engine)
